63d40f4d6147151207fbb1f92201ee2e0157323b
diegolins10 <dede140126>
2022-08-30 20:26:33 -0300

    criação de dtos

---
+++ SendEmailAPI/Models/AddPackageInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendEmailAPI.Models
+{
+    public class AddPackageInputModel
+    {
+        public string Title { get; set; }
+        public decimal Weight { get; set; }
+    }
+}
---
+++ SendEmailAPI/Models/AddPackageUpdateInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendEmailAPI.Models
+{
+    public class AddPackageUpdateInputModel
+    {
+        public string Status { get; set; }
+        public bool Delivered { get; set; }
+    }
+}
