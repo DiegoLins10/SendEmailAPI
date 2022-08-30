462aa24a82472e4729b2ed34b10d52ffce25a125
diegolins10 <dede140126>
2022-08-30 20:26:22 -0300

    crianção context persistencia de dados

---
+++ SendEmailAPI/Persistence/EmailContext.cs
@@ -0,0 +1,18 @@
+﻿using SendEmailAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendEmailAPI.Persistence
+{
+    public class EmailContext
+    {
+        public List<Package> Packages { get; set; }
+
+        public EmailContext()
+        {
+            Packages = new List<Package>();
+        }
+    }
+}
